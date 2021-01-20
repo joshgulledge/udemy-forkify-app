ad85db374f1d4f1e64f7ee4c87777cddb0713ed0
Josh Gulledge <joshcalebgulledge@gmail.com>
2021-01-19 20:37:57 -0600

    search results done

--- src/js/controller.js
+++ src/js/controller.js
@@ -6,6 +6,10 @@ import resultsView from './views/resultsView.js';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
+if (module.hot) {
+  module.hot.accept();
+}
+
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
@@ -34,7 +38,6 @@ const controlSearchResults = async function () {
 
     await model.loadSearchResults(query);
 
-    console.log(model.state.search.results);
     resultsView.render(model.state.search.results);
   } catch (err) {
     console.log(err);
--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -3,6 +3,8 @@ import icons from 'url:../../img/icons.svg';
 
 class ResultsView extends View {
   _parentEl = document.querySelector('.results');
+  _errorMessage = `The All Powerful search came back with no results. Spell Check? 💥`;
+  _message = ``;
 
   _generateMarkup() {
     console.log(this._data);
@@ -13,18 +15,13 @@ class ResultsView extends View {
   _generateMarkupPreview(result) {
     return `
         <li class="preview">
-            <a class="preview__link preview__link--active" href="#${result.id}">
+            <a class="preview__link" href="#${result.id}">
                 <figure class="preview__fig">
                     <img src="${result.image}" alt="${result.title}" />
                 </figure>
                 <div class="preview__data">
                     <h4 class="preview__title">${result.title}</h4>
                     <p class="preview__publisher">${result.publisher}</p>
-                    <div class="preview__user-generated">
-                        <svg>
-                            <use href="${icons}#icon-user"></use>
-                        </svg>
-                    </div>
                 </div>
             </a>
         </li>
